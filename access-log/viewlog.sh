cfc72913ee1ed502661bb84360bc4ed3502a9a9f
Luke Bettner <94314593+InquireWithin@users.noreply.github.com>
2022-12-04 19:11:47 -0500

    Update viewlog.sh

--- access-log/viewlog.sh
+++ access-log/viewlog.sh
@@ -8,5 +8,3 @@ cat $1.rpt | sed -E -e '1,7d' -e '180,187d' | sed -E -e 's/.*\s.*\s0//g' -e '/./
 #summary
 cat $1.rpt | sed -nE -e '180,187p'
 
-#./ac_summary.awk access_log.log | sed -E s/\t[0]$// #remove 0s
-#./ac_summary.awk access_log.log | sed -E -e '1,7d' -e '180,187d' | sed -E s/.*\s[0]$// #improved remove 0 & header remover
